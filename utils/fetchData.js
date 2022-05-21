9a578c62baf7ed72bf4cc3889222dfaa598261d3
Ahmed Chawshin <ahmed.n.chawshin@hiof.no>
2022-05-22 00:06:07 +0200

    fixed issues

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -12,18 +12,16 @@ const fetchData = (url) => {
     })
     .then((res) => {
       let data = res.data.data;
-      let skins = res.data.data;
 
       console.log(
         `Status: ${res.data.code}. ${data.page_num}/${data.total_page}`
       );
-
       for (let i = 0; i < data.items.length; i++) {
         arrPrices.push({
-          name: skins.items[i].market_hash_name,
-          price: `${parseFloat(skins.items[i].quick_price / config.conversion_rate).toFixed(2)}`,
-          steam_market_url: skins.items[i].steam_market_url,
-          has_history_on_buff : skins.items[i].has_buff_price_history
+          name: data.items[i].market_hash_name,
+          price: `${parseFloat(data.items[i].quick_price / config.conversion_rate).toFixed(2)}`,
+          steam_market_url: data.items[i].steam_market_url,
+          has_history_on_buff : data.items[i].has_buff_price_history
         });
       }
       if (data.page_num === data.total_page) {
