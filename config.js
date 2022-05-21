8b787d846376b4f63267efab5c914a6fb6ca0304
Ahmed Chawshin <ahmed.n.chawshin@hiof.no>
2022-05-22 00:42:51 +0200

    removed page_size because it will auto fetch

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 module.exports = {
     buffurl: "https://buff.163.com/api/market/goods?game=csgo&page_size=100&page_num=",
-    page_size: 240,
     conversion_rate: 7.03571429,
     cookie: process.env.COOKIES
   }
